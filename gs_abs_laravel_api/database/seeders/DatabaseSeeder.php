12c4f519bee6d13b958a6473da4a390c4ae632e7
Bilal <bilalowraiylegh@gmail.com>
2025-08-12 12:56:07 +0200

    adding sidebar

--- gs_abs_laravel_api/database/seeders/DatabaseSeeder.php
+++ gs_abs_laravel_api/database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,8 +17,11 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
+            'nom' => 'kamali',
+            'prenom' => 'ahmed',
             'email' => 'test@example.com',
+            'password' => Hash::make('1234'),
+            'role' => 'surveillant'
         ]);
     }
 }
